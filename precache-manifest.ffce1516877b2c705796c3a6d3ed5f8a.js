87acdafb1e1130e7d65b9c3ac5d8c628a078d049
tasir170 <66586729+tasir170@users.noreply.github.com>
2020-07-26 11:41:15 +0500

    Add files via upload

---
+++ precache-manifest.ffce1516877b2c705796c3a6d3ed5f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68dbf9fb0d0fe7f0840814e852b9e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d033639e9258a6226189",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a398a19afcae720caa05",
+    "url": "/static/js/2.28e0a7bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28e0a7bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d033639e9258a6226189",
+    "url": "/static/js/main.7c3347f3.chunk.js"
+  },
+  {
+    "revision": "a8da3935970330e26899",
+    "url": "/static/js/runtime-main.d0c8e23a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
